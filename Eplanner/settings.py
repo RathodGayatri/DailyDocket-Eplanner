5bb1b7790b3d5bbb918a7a9b588d2e69a8805522
RathodGayatri <gayatrirathod1803@gmail.com>
2026-08-08 18:16:31 +0530

    Fix allowed hosts for deployment

--- Eplanner/settings.py
+++ Eplanner/settings.py
@@ -10,10 +10,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["dailydocket-eplanner.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
